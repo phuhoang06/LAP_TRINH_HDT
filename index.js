e16d4c794a10d3f3d4b27eb7d018ca430b318ab3
phuhoang.06 <hoangvanphu2006@gmail.com>
2024-10-22 08:48:59 +0700

    add

---
+++ index.js
@@ -0,0 +1,33 @@
+let Mydate = function (day, month, year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+    this.getDay = function (){
+        return this.day;
+    }
+    this.getMonth = function (){
+        return this.month;
+    }
+    this.getYear = function (){
+        return this.year;
+    }
+    this.setDay = function(day) {
+        this.day  = day;
+    }
+    this.setMonth = function(month) {
+        this.month  = month;
+    }
+    this.setYear = function(year) {
+        this.year  = year;
+    }
+};
+let date = new Mydate(2, 2, 2007);
+date.setDay(10);
+date.setMonth(10);
+date.setYear(2019);
+let day = date.getDay(); // 2
+
+let month = date.getMonth(); // 2
+
+let year = date.getYear(); // 2007
+alert(day + "/" + month + "/" + year);
